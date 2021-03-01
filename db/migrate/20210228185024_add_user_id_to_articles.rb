ab2cb443fd271aff6fd5f60be7c26e553f784386
櫻井直輝 <urimuuuofficial@gmal.com>
2021-03-01 11:22:08 +0900

    間違えて消した内容を復元

--- db/migrate/20210228185024_add_user_id_to_articles.rb
+++ db/migrate/20210228185024_add_user_id_to_articles.rb
@@ -1,6 +1,7 @@
 class AddUserIdToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :articles, :user
-    # add_column :articles, :user_id, :integer
+    def change
+      add_reference :articles, :user
+      # add_column :articles, :user_id, :integer
+    end
   end
-end
+  
