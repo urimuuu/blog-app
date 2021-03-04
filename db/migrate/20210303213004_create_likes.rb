3f16aad705b3dc9bf03ffc5bff970c8db8390af8
櫻井直輝 <urimuuuofficial@gmal.com>
2021-03-04 16:51:18 +0900

    いいね解除

--- db/migrate/20210303213004_create_likes.rb
+++ db/migrate/20210303213004_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-      t.references :user
+      t.references :user, null: false
       t.references :article, null: false
       t.timestamps
     end
